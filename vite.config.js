b7f5d69df5272f8fb030d4b7d09d0ab4642b0a1c
BalwinderCa <balwinder98.ca@gmail.com>
2024-07-16 17:27:48 -0600

    psushing updates

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,23 @@
 import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import DefineOptions from 'unplugin-vue-define-options/vite'
-import fs from 'fs';
-import { resolve } from 'path';
-import { homedir } from 'os';
+import DefineOptions from 'unplugin-vue-define-options/vite';
 
-export default defineConfig(({command, mode}) => {
+export default defineConfig(({ command, mode }) => {
     // Load current .env-file
-    const env = loadEnv(mode, process.cwd(), '')
-
-    // Set the host based on APP_URL
-    let host = env.APP_URL !== undefined ? new URL(env.APP_URL).host : null;
-    let homeDir = homedir()
-    let serverConfig = {}
-
-    if (host && homeDir) {
-        const certificatesPath = env.CERTIFICATES_PATH !== undefined ? env.CERTIFICATES_PATH : `.config/valet/Certificates/${host}`;
-
-        serverConfig = {
-            https: {
-                key: fs.readFileSync(
-                    resolve(homeDir, `${certificatesPath}.key`),
-                ),
-                cert: fs.readFileSync(
-                    resolve(homeDir, `${certificatesPath}.crt`),
-                ),
-            },
-            hmr: {
-                host
-            },
-            host
-        }
-    }
+    const env = loadEnv(mode, process.cwd(), '');
 
     return {
+        server: {
+            port: 3000, // Configure Vite to run on port 3000
+        },
         publicDir: 'vendor/mixpost',
         plugins: [
             laravel({
                 input: 'resources/js/app.js',
                 publicDirectory: 'resources/dist',
                 buildDirectory: 'vendor/mixpost',
-                refresh: true
+                refresh: true,
             }),
             vue({
                 template: {
@@ -51,14 +27,13 @@ export default defineConfig(({command, mode}) => {
                     },
                 },
             }),
-            DefineOptions()
+            DefineOptions(),
         ],
         resolve: {
             alias: {
                 '@css': '/resources/css',
-                '@img': 'resources/img'
+                '@img': 'resources/img',
             },
         },
-        server: serverConfig
-    }
+    };
 });
